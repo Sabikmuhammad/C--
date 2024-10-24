4d4d18ab3740123708dd6d2f5e428b9b788157f9
Muhammad Sabik <muhammadsabik.org@gmail.com>
2024-10-24 10:28:12 +0530

    deleted java temp files

--- tempCodeRunnerFile.java
+++
@@ -1,8 +0,0 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        int i=100,j=200;
-        while(++i<--j);
-            
-        System.out.println("mid point is"+i);
-    }
-}
